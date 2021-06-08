ee1d19bcf4f399200f3bfeae78bfce134d27d4e3
helloteacher-alt <panyisa20012544@gmail.com>
2021-06-08 23:32:30 +0700

    finish

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _22222Homework6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("The cost of x:");
+            int xrates = int.Parse(Console.ReadLine());
+            Console.Write("The cost of y:");
+            int yrates = int.Parse(Console.ReadLine());
+            Console.Write("Input petrol's price of x:");
+            int petrolX = int.Parse(Console.ReadLine());
+            Console.Write("Input petrol's price of y:");
+            int petrolY = int.Parse(Console.ReadLine());
+            x = xrates + petrolX;
+            y = yrates + petrolY;
+            if (x > y)
+            {
+                Console.WriteLine("2nd is chaper");
+            }else if (x < y)
+            {
+                Console.WriteLine("1st is cheaper");
+            }else if (x == y)
+            {
+                Console.WriteLine("Same cost");
+            }
+        }
+    }
+}
